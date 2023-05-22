9816b836266db6ec1dd647db82fdece10477c61f
realvudi <91071707+realvudi@users.noreply.github.com>
2023-05-22 06:16:30 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Main from './pages/Main';
 import Navbar from './pages/Navbar';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Menu from './pages/Menu';
 import Basket from './pages/Basket';
 import Contacts from './pages/Contacts';
